39869e84437b3d4384baddc6c4414d2826ce8af6
davalarcon <zilentstorm@gmail.com>
2017-08-18 14:47:52 -0400

    delete option

--- todo.js
+++ todo.js
@@ -4,14 +4,18 @@ var input = prompt('what would you like to do?');
 
 while (input !== 'quit') {
     if (input === 'list') {
-        todos.forEach((todo) => {
+        todos.forEach((todo, i) => {
             console.log('***********');
-            console.log(todo);
-
-        }, this);
+            console.log(i + ': ' + todo);
+        });
+        console.log('**************');
     } else if (input === 'new') {
         var newTodo = prompt('enter new to do, please.');
         todos.push(newTodo);
+    } else if (input === 'delete') {
+        var index = prompt('which index to be deleted?');
+        todos.splice(index, 1);
+        console.log('Todo deleted');
     }
     input = prompt('what would you like to do?');
 }
